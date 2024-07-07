033fc3e83d2c443a82acf5310dca54b1b5fdf7c5
zakaria.narjis <zakaria.narjis.97@gmail.com>
2024-07-07 16:08:39 +0200

    add resize option fiveK dataset

--- envs/env_dataloader.py
+++ envs/env_dataloader.py
@@ -13,9 +13,9 @@ class PhotoEnhancement(Dataset):
         Encode dataset images
         output : torch.Tensors of encoded and raw source/target images (3,H,W)
     """
-    def __init__(self,image_size,mode = 'train', pre_encode = True) -> None:
+    def __init__(self,image_size,mode = 'train', pre_encode = True,resize=True) -> None:
         super().__init__()
-        self.img_dataset = FiveKDataset(mode=mode,image_size=image_size)
+        self.img_dataset = FiveKDataset(mode=mode,image_size=image_size,resize=resize)
         self.img_dataloader = DataLoader(self.img_dataset , batch_size=ENCODING_BATCH_SIZE, shuffle=False)
         self.pre_encode = pre_encode
         if self.pre_encode == True:
@@ -47,12 +47,12 @@ class PhotoEnhancement(Dataset):
             return source_image,target_image
     
 
-def create_dataloaders(batch_size,image_size,train=True,pre_encode= True,shuffle=True):
+def create_dataloaders(batch_size,image_size,train=True,pre_encode= True,shuffle=True,resize=True):
     if train:    
-        train_dataset = PhotoEnhancement(image_size=image_size,mode='train',pre_encode=pre_encode)
+        train_dataset = PhotoEnhancement(image_size=image_size,mode='train',pre_encode=pre_encode,resize=resize)
         dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle = shuffle)
     else: 
-        test_dataset = PhotoEnhancement(image_size=image_size,mode='test', pre_encode = pre_encode)
+        test_dataset = PhotoEnhancement(image_size=image_size,mode='test', pre_encode = pre_encode,resize=resize)
         dataloader = DataLoader(test_dataset, batch_size=batch_size , shuffle = shuffle)
 
     return dataloader
--- envs/image_dataset.py
+++ envs/image_dataset.py
@@ -16,12 +16,12 @@ class FiveKDataset(Dataset):
     A dataset that reads Adobe5K dataset images
     output : tensor of unprocessed images
     """
-    def __init__(self, image_size,mode="train", ):
+    def __init__(self, image_size,mode="train", resize= True ):
         if mode =='train':
             self.IMGS_PATH = "./dataset/FiveK/train/"
         else:
             self.IMGS_PATH = "./dataset/FiveK/test/"
-
+        self.resize= resize
         self.transform = transforms.Compose([
             v2.Resize(size = (image_size,image_size), interpolation= transforms.InterpolationMode.BICUBIC),
         ])
@@ -34,8 +34,12 @@ class FiveKDataset(Dataset):
     
     def __getitem__(self,idx):
         source_path = self.img_files[idx]
-        source = self.transform(read_image(self.IMGS_PATH+'input/'+source_path))
-        target = self.transform(read_image(self.IMGS_PATH+'target/'+source_path))
+
+        source = read_image(self.IMGS_PATH+'input/'+source_path)
+        target = read_image(self.IMGS_PATH+'target/'+source_path)
+        if self.resize:
+            source = self.transform(source)
+            target = self.transform(target)
         # source = self.transform(Image.open(ORIGINAL_FOLDER+source_path))
 
         return source, target 
