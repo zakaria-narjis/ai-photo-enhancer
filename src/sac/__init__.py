364e826c736170007ae9ad5ce0dff93d29598799
zakaria.narjis <zakaria.narjis.97@gmail.com>
2024-09-15 13:02:30 +0200

    update benchmark notebook

--- src/sac/__init__.py
+++ src/sac/__init__.py
@@ -1,7 +1,7 @@
-# from src.sac.sac_algorithm import SAC  # noqa F401
-# from src.sac.sac_networks import Actor, SoftQNetwork  # noqa F401
-# from src.sac.sac_inference import InferenceAgent  # noqa F401
-# from src.sac.utils import save_actor_head  # noqa F401
-# from src.sac.utils import load_actor_head  # noqa F401
-# from src.sac.utils import save_critic_head  # noqa F401
-# from src.sac.utils import load_critic_head  # noqa F401
+from src.sac.sac_algorithm import SAC  # noqa F401
+from src.sac.sac_networks import Actor, SoftQNetwork  # noqa F401
+from src.sac.sac_inference import InferenceAgent  # noqa F401
+from src.sac.utils import save_actor_head  # noqa F401
+from src.sac.utils import load_actor_head  # noqa F401
+from src.sac.utils import save_critic_head  # noqa F401
+from src.sac.utils import load_critic_head  # noqa F401
